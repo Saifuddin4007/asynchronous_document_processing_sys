3eb9143097d27583cfac19a7028d8b7932a55d7d
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-05-13 22:58:44 +0530

    Updated entry point

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pool from './src/db/pool.js';
+import {errorHandler} from '../server/src/middlewares/errorHandler.js';
+import documentRoutes from './src/routes/documentRoutes.js';
+const app= express();
+
+
+dotenv.config();
+
+
+
+const PORT= process.env.PORT || 7001;
+
+//!express-middleware
+app.use(express.json());
+
+
+//!CORS
+app.use(cors({
+    origin: 'http://localhost:7001', // your React app's exact origin
+    credentials: true,               // required because axios has withCredentials: true
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}));
+
+
+//!Routes
+app.use('/api/v1', documentRoutes);
+// app.use('/api/v1', jobRoutes);
+// app.use('/api/v1', resultRoutes);
+// app.use('/api/v1', exportRoutes);
+
+
+//!Always last
+app.use(errorHandler);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is started at PORT: ${PORT}`);
+})
