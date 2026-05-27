55ecd2dd6d4db7b3a1c1113bc183e99e3cddec2a
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-05-27 21:21:15 +0530

    Added api file through which client connects with server and communicates with server through api calls

---
+++ client/src/services/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const api= axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+})
+
+export const uploadDocument= async (file)=>{
+    const formData= new FormData();
+    formData.append('file', file);
+
+    const res= await api.post('/api/v1/documents/upload', formData);
+    console.log(res.data);
+    return res.data;
+}
+
+export const getJobStatus= async (documentId)=>{
+    const res= await api.get(`/api/v1/jobs/${documentId}`);
+    return res.data;
+}
+
+export const getResults= async (documentId)=>{
+    const res= await api.get(`/api/v1/results/${documentId}`);
+    return res.data;
+} 
+
+export const saveReviewedData= async (docId, reviewedData, isFinalized=false)=>{
+    const res= await api.patch(`/api/v1/results/${docId}`, {
+        reviewed_data: reviewedData,
+        is_finalized: isFinalized
+    });
+    return res.data;
+}
