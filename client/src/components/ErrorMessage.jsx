3006902a97589c1a9cf44755da24882dcdaa2908
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-05-27 21:22:48 +0530

    Added ErrorMessage component to handle all errors throughout the UI

---
+++ client/src/components/ErrorMessage.jsx
@@ -0,0 +1,14 @@
+
+
+const ErrorMessage = ({ message }) => {
+
+    return (
+
+        <div className="bg-red-50 text-sm text-red-700 max-w-3xl mx-auto mt-5 rounded-md p-4">
+            {message}
+        </div>
+
+    )
+}
+
+export default ErrorMessage
