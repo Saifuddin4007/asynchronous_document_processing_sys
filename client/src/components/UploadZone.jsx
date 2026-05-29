65e0ebef1c44d23668ba66e4aac08d2eca9334db
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-05-29 18:55:48 +0530

    Updated uttons

--- client/src/components/UploadZone.jsx
+++ client/src/components/UploadZone.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react"
-import FilePreview from "./FilePreview";
+
 
 
 
@@ -42,11 +42,10 @@ const UploadZone = ({handleUpload, handleFileSelect, handleRemoveFile, upload, s
     <div className={`flex justify-center items-center border-2 border-dashed ${isDragging ? 'border-green-500 bg-green-50' : 'border-gray-300'}`} onDragOver={handleOnDragOver} onDragLeave={handleOnDragLeave} onDrop={handleOnDrop}>
         <input ref={inputRef} type="file" className="hidden" onChange={(e)=>handleFileSelect(e.target.files[0])}/>
         <button className="bg-gray-300 px-1.5 py-0.5 mx-2 my-5 rounded-sm text-black" onClick={openFilePicker}>Browse File</button>
-        <button className="bg-blue-500 px-3 py-1 mx-3 my-5 rounded-md text-black " onClick={handleUpload} disabled={upload}>Upload</button>
-        <button className="bg-blue-500 px-3 py-1 mx-3 my-5 rounded-md text-black " onClick={handleRemove} disabled={upload}>Clear</button>
+        {selectedFile &&  <button className="bg-blue-500 px-3 py-1 mx-3 my-5 rounded-md text-black " onClick={handleUpload} disabled={upload}>Upload</button>}   
+        {selectedFile && <button className="bg-blue-500 px-3 py-1 mx-3 my-5 rounded-md text-black " onClick={handleRemove} disabled={upload}>Clear</button>}    
         
-    </div> 
-    <FilePreview selectedFile={selectedFile}/>
+    </div>
     </>
        
   )
